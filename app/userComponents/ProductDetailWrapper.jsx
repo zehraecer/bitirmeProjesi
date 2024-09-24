5812fb771850d760e073466dae8da9c6811419c1
Zehra Ecer <zehraecer1996@hotmail.com>
2024-09-24 21:57:01 +0300

    sepete ekle fonksiyonunun bir kısmı yazıldı

--- app/userComponents/ProductDetailWrapper.jsx
+++ app/userComponents/ProductDetailWrapper.jsx
@@ -1,11 +1,21 @@
+import { useMyContext } from "../context";
+
 export const ProductDetailWrapper = ({ clickedProduct }) => {
+    const { Products } = useMyContext()
+
+    const AddToCart = (element) => {
+        console.log(element);
+        const clickedProduct = Products.find(product => product.id === element)
+        console.log(clickedProduct);
+    }
 
     return (
-
         <>
             <span>{clickedProduct.description}</span>
             <img style={{ width: "100px", height: "100px" }} src={clickedProduct.product_img} />
             <span>{clickedProduct.price}₺</span>
+
+            <button onClick={() => AddToCart(clickedProduct.id)}>Sepete ekle</button>
         </>
     )
 }
