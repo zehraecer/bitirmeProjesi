2f1977c85e591550d09d5d2c60cf055d160bb233
Zehra Ecer <zehraecer1996@hotmail.com>
2024-10-03 21:43:06 +0300

    build hataları çözüldü-10

--- utils/supabase/client.js
+++ utils/supabase/client.js
@@ -1,8 +1,7 @@
-import { createBrowserClient } from '@supabase/ssr';
+import { createClient } from '@supabase/supabase-js';
 
-export function createClient() {
-    return createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    );
-}
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// Supabase istemcisi oluşturma
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
